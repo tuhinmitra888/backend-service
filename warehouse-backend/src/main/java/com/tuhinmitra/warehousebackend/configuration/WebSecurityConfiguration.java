8dc09deac352f77a3a4ae8d88328539a38e6a97a
Mitra <tuhin.mitra@postnl.nl>
2021-03-25 22:03:46 +0100

    Fix 403 authorization errors

--- warehouse-backend/src/main/java/com/tuhinmitra/warehousebackend/configuration/WebSecurityConfiguration.java
+++ warehouse-backend/src/main/java/com/tuhinmitra/warehousebackend/configuration/WebSecurityConfiguration.java
@@ -20,6 +20,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
                 .authorizeRequests().antMatchers("/api/**").authenticated()
                 .and()
                 .httpBasic()
