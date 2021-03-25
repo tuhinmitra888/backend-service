67e29ab0195817970cf0d4414d2823c3dbf4d471
Mitra <tuhin.mitra@postnl.nl>
2021-03-25 21:37:31 +0100

    Security - basic authorization of endpoints is implemented

---
+++ warehouse-backend/src/main/java/com/tuhinmitra/warehousebackend/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.tuhinmitra.warehousebackend.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/api/**").authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        String username = "user";
+        String password = "password";
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        String encodedPassword = passwordEncoder().encode(password);
+        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
