53fe8b73c16262357fe2488566d853c9cffa9d09
Mitra <tuhin.mitra@postnl.nl>
2021-03-25 17:43:38 +0100

    Initial set up the backend of the backend project

---
+++ warehouse-backend/src/test/java/com/tuhinmitra/warehousebackend/WarehouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuhinmitra.warehousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
